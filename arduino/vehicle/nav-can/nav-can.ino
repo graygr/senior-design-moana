7d31bbd301590f1f4da6176e9b5fedddfbadb38e
= <=>
2021-01-31 17:33:58 -0800

    updated with i2c code

--- arduino/vehicle/nav-can/nav-can.ino
+++ arduino/vehicle/nav-can/nav-can.ino
@@ -1,22 +1,51 @@
-/* This sketch waits for incoming serial data to be present
-   and then sends a message serially.
-   
-   After Serial.println() is a call to Serial.read() to remove
-   the received byte from the input queue. If you send more than 
-   one byte to the Arduino, you'll get more than one response sent
-   back at you.
-*/
+#include <Wire.h>
 
-void setup()
-{
+#define TCAADDR 0x70
+
+int clockSpeed = 100000;
+char myOutput[32] = {0};
+char sent[32] = {0};
+
+void setup() {
+  Wire.begin(0x04);
+  Wire.setClock(clockSpeed);
+  Wire.onReceive(receiveEvent);
   Serial.begin(9600);
+  tcaselect(1);
 }
 
-void loop()
-{
-  if (Serial.available() > 0)
-  {
-    Serial.println("Hello");
-    Serial.read();
+void loop() {
+  delay(100);
+  if(strcmp(myOutput, sent)) {
+    send(myOutput);
+    strncpy(sent, myOutput, 32);
   }
+}
+
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();
+}
+
+void receiveEvent() {
+  char c[32] = {0};
+  int count = 0;
+  while (0 < Wire.available()) {
+    c[count] = Wire.read();
+    Serial.print(c[count]);
+    count++;
+  }
+  Serial.println();
+  strncpy(myOutput, c, 32);
+}
+
+void send(char c[]) {
+  Wire.beginTransmission(0x40);
+  Serial.print("Sending signal:\t");
+  Wire.write(c);
+  Serial.print(c);
+  Serial.println();
+  Wire.endTransmission();
 }
---
+++ gcs/groundcontrol/source/i2c.h
@@ -0,0 +1,8 @@
+#ifndef I2C_H
+#define I2C_H
+
+#include <string>
+
+bool writeStream(int file_i2c, string output);
+
+#endif /* I2C_H */
