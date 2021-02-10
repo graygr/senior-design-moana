f79c1c9c4dba02a59a1c154792c8aaf3fff88d93
graygr <glimstrom@scu.edu>
2021-02-09 18:06:34 -0800

    cleaned up junk cpp code

---
+++ jetson/navcomms/src/i2c.h
@@ -0,0 +1,8 @@
+#ifndef I2C_H
+#define I2C_H
+
+#include <string>
+
+bool writeStream(int file_i2c, std::string output);
+
+#endif /* I2C_H */
---
+++ jetson/navcomms/src/uart.h
@@ -0,0 +1,18 @@
+/* 
+    File: uart.h / uart.cpp
+    Author: Gregor Limstrom
+    Contact: glimstrom@scu.edu
+    Last modified: 1/31/2021
+
+    Purpose: This file contains code to send and receive commands over UART serial between the arduino
+        and the jetson nano.
+
+    Based off this code: https://github.com/cheydrick/Canonical-Arduino-Read/blob/master/canonicalarduinoread.c#L15
+ */
+
+#ifndef UART_H
+#define UART_H
+
+// TODO: Figure out what functions we need in header file, if at all
+
+#endif /* UART_H */
